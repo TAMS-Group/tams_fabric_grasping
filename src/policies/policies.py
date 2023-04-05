0caa5549af4fcf2e7f21421bf752174e8ce24fe4
Niklas Fiedler <git@nfiedler.de>
2023-04-05 13:49:35 +0200

    tactile grasp policies

--- src/policies/policies.py
+++ src/policies/policies.py
@@ -65,8 +65,8 @@ class PositionPolicy(Policy):
         if msg.sensor_id == 2:
             self.tactile2 = msg.data
 
-    def gripper_current_cb(self, goal: float):
-        self.gripper_current = goal
+    def gripper_current_cb(self, current: float):
+        self.gripper_current = current
 
     def force_cb(self, msg: Joy):
         self.force = msg
@@ -142,3 +142,97 @@ class GripperCurrentPolicy(Policy):
         return self.gripper_current.size > 9 and abs(self.gripper_current - self.overall_gripper_current_goal).sum() < self.gripper_current.size * 100
 
 
+
+
+class TactilePolicy(Policy):
+
+    def __init__(self, cell_active_threshold: int=900, max_gripper_current: int=1800, max_cell_loss: int=4):
+        self.gripper_move_speed = 0.005
+        self.cell_active_threshold = cell_active_threshold
+        self.max_gripper_current = max_gripper_current
+        self.max_cell_loss = max_cell_loss
+        self.reset()
+
+    def reset(self):
+        self.overall_gripper_goal = 0
+        self.gripper_goal = None
+        self.gripper_current = None
+        self.state = None
+        self.tactile1 = None
+        self.tactile2 = None
+        self.torque = None
+        self.force = None
+        self.tactile_results = dict()
+        self.explore = True
+    
+    def num_cells_over_threshold(self):
+        if self.tactile1 is not None and self.tactile2 is not None:
+            return (np.array(self.tactile1 + self.tactile2) > self.cell_active_threshold).sum()
+        return 0
+
+    def tactile_cb(self, msg: TactileSensorArrayData):
+        if msg.sensor_id == 1:
+            self.tactile1 = 1023 - np.array(msg.data)
+        if msg.sensor_id == 2:
+            self.tactile2 = 1023 - np.array(msg.data)
+
+    def gripper_current_cb(self, current: float):
+        if self.explore:
+            if self.max_gripper_current < current:
+                # max load reached, stop exploring
+                self.explore = False
+                print(self.tactile_results)
+
+                # set position with maximal tactile contact as goal
+                self.overall_gripper_goal = self.tactile_results[max(self.tactile_results.keys())]
+        self.gripper_current = current
+
+    def force_cb(self, msg: Joy):
+        self.force = msg
+
+    def torque_cb(self, msg: Joy):
+        self.torque = msg
+
+    def state_cb(self, msg: CartesianState):
+        self.state = msg
+
+    def gripper_goal_cb(self, goal: int):
+        if self.explore:
+            cells = self.num_cells_over_threshold()
+            if cells not in self.tactile_results:
+                self.tactile_results[cells] = goal
+            elif cells in self.tactile_results and self.tactile_results[cells] < goal:
+                self.tactile_results[cells] = goal
+        self.gripper_goal = goal
+
+    def decide_action(self) -> int:
+        print(self.num_cells_over_threshold())
+        # close further and further when exploring
+        if self.explore: 
+            return 0.5
+
+        cellcount = self.num_cells_over_threshold()
+        # opened too far, start again
+        if cellcount < 2:
+            self.reset()
+        """
+        #  navigate to previously decided goal
+        if self.gripper_goal and self.gripper_goal < self.overall_gripper_goal:
+            return 0.5
+        if self.gripper_goal and self.gripper_goal > self.overall_gripper_goal:
+            return -0.5
+        """
+
+        # open up after exploration until satisfying result is reached
+        if cellcount + self.max_cell_loss < max(self.tactile_results.keys()):
+            return -0.5
+
+        return 0
+
+    def finished(self) -> bool:
+        # stop when close enough to goal and exploration is done.
+        # return not self.explore and self.gripper_goal and abs(self.gripper_goal - self.overall_gripper_goal) < 0.01
+        done = not self.explore and self.num_cells_over_threshold() + self.max_cell_loss >= max(self.tactile_results.keys())
+        if done:
+            print(f'finishing with {self.num_cells_over_threshold()} cells after maximum with {max(self.tactile_results.keys())} cells')
+        return done
