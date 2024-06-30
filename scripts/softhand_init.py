702bc590252e91acde2d1e397c0e3b01a09d5226
Niklas Fiedler <git@nfiedler.de>
2024-06-30 18:12:56 +0200

    cleanup

--- scripts/softhand_init.py
+++
@@ -1,6 +0,0 @@
-import rospy
-
-from controller_manager_msgs.srv import SwitchController
-
-        rospy.wait_for_service('/controller_manager/switch_controller')
-        self.switch_controllers = rospy.ServiceProxy('/controller_manager/switch_controller', SwitchController)
